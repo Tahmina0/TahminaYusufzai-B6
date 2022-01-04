f7040424c50380551d6cee58c87cabc2b91ed4c6
Tahmina <tahminaahmad12@icloud.com>
2022-01-04 16:16:03 -0500

    day 10 HomeWork 3

---
+++ src/test/java/com/noorteck/java/hw10/Method3.java
@@ -0,0 +1,27 @@
+package com.noorteck.java.hw10;
+
+public class Method3 {
+	public static void main (String []args) {
+		oddEven(2);
+		oddEven(3);
+		oddEven(4);
+		oddEven(5);
+	}
+
+	public static void oddEven(int number) {
+		
+		if ((number%2) == 0 ) 
+			System.out.println("the number is an even number");
+		
+		else 
+			System.out.println("the number is an odd number");
+			
+		}
+		
+			
+
+	}
+	
+	
+	
+
