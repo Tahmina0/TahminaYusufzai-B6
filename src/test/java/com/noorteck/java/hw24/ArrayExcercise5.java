b0c3bd6dab2e9984975c7f7442f92f4de0e29ae4
Tahmina <tahminaahmad12@icloud.com>
2022-02-07 13:36:38 -0500

    Broken code Homework

---
+++ src/test/java/com/noorteck/java/hw24/ArrayExcercise5.java
@@ -0,0 +1,35 @@
+package com.noorteck.java.hw24;
+
+public class ArrayExcercise5 {
+	
+	
+	public static void main (String []args) {
+		
+	}
+	
+		
+		
+		
+		
+		
+		int[] getFirstLast(int[]number) {
+			
+			int[] result= new  int [1] ;
+			
+			int firstElemenet=number[0]; 
+			
+			int lastElement1=number[number.length-1]; 
+			
+				
+			
+			result[0]= firstElemenet; 
+			result[1]= lastElement1; 
+			
+			
+			
+			return result; 
+			
+			
+		}
+		
+	}
